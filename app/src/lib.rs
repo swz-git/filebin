d78fafea680776c6f9e15a4a2f1321a8885ed241
swz-git <contact@swz.works>
2025-08-13 23:28:56 +0200

    uhm

--- app/src/lib.rs
+++ app/src/lib.rs
@@ -1,4 +1,4 @@
-use leptos::prelude::*;
+use leptos::{leptos_dom::logging::console_log, prelude::*};
 use leptos_meta::{provide_meta_context, MetaTags, Stylesheet, Title};
 use leptos_router::{
     components::{Route, Router, Routes},
@@ -29,7 +29,7 @@ pub fn App() -> impl IntoView {
     provide_meta_context();
 
     view! {
-        <Stylesheet id="leptos" href="/pkg/start-axum-workspace.css"/>
+        <Stylesheet id="leptos" href="/pkg/filebin.css"/>
 
         // sets the document title
         <Title text="Welcome to Leptos"/>
@@ -37,7 +37,7 @@ pub fn App() -> impl IntoView {
         // content for this welcome page
         <Router>
             <main>
-                <Routes fallback=|| "Page not found.".into_view()>
+                <Routes fallback=NotFound>
                     <Route path=StaticSegment("") view=HomePage/>
                 </Routes>
             </main>
@@ -47,10 +47,22 @@ pub fn App() -> impl IntoView {
 
 /// Renders the home page of your application.
 #[component]
+fn NotFound() -> impl IntoView {
+    view! {
+        <h1>"404: Not Found"</h1>
+    }
+}
+
+/// Renders the home page of your application.
+#[component]
 fn HomePage() -> impl IntoView {
     // Creates a reactive value to update the button
     let count = RwSignal::new(0);
-    let on_click = move |_| *count.write() += 1;
+    console_log("Initing...");
+    let on_click = move |_| {
+        console_log("Hello!");
+        *count.write() += 1
+    };
 
     view! {
         <h1>"Welcome to Leptos!"</h1>
